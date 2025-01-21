286a05cba88246940466b51d3bc701d8a295fa23
nmktad <nmktadesse@gmail.com>
2025-01-21 10:49:23 +0300

    stylix image

--- nixos/modules/stylix/default.nix
+++ nixos/modules/stylix/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: 
+{ pkgs, ... }:
 
 let
   opacity = 0.95;
@@ -8,6 +8,10 @@ in
   stylix = {
     enable = true;
     polarity = "dark";
+    image = pkgs.fetchurl {
+      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+    };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
     # fonts = {
